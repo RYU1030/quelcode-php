386f1f4edc1ed4994f32d1080af66f5254df4c5b
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2020-05-07 20:31:04 +0900

    first push

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,11 +2,17 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-
+$length = sizeof($array);
+for ($i = 0; $i < $length; $i++) {
+  for ($j = 0; $j < $length - 1 - $i; $j++) {
+    if ($array[$j] > $array[$j + 1]) {
+      $temp = $array[$j];
+      $array[$j] = $array[$j + 1];
+      $array[$j + 1] = $temp;
+    }
+  }
 }
 // 修正はここまで
-
 echo "<pre>";
 print_r($array);
 echo "</pre>";
