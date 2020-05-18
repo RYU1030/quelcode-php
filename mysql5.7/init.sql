183377105c73a3cd75776c756cd57f2fdec90fdc
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2020-05-18 17:44:42 +0900

    init.sqlをプッシュ

--- mysql5.7/init.sql
+++ mysql5.7/init.sql
@@ -10,14 +10,22 @@ CREATE TABLE `members` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE `posts` (
-      `id` int(11) NOT NULL AUTO_INCREMENT,
-      `message` text NOT NULL,
-      `member_id` int(11) NOT NULL,
-      `reply_post_id` int(11) NOT NULL,
-      `created` datetime NOT NULL,
-      `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-      PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+  `id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `reply_post_id` int(11) NOT NULL,
+  `retweeted_post_id` int(11) NOT NULL,
+  `retweeted_by` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleteflag` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `likes` (
+  `liked_post_id` int(11) NOT NULL,
+  `liked_by` int(11) NOT NULL,
+  `deleteflag` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `prechallenge3` (
       `value` int(11) NOT NULL
