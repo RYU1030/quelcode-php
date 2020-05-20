49ee677bdcfb5cc5d7cd5dea2e86186cdc7f11c1
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2020-05-20 12:45:08 +0900

    delete.php ファイルの整形。

--- html/php-challenge/delete.php
+++ html/php-challenge/delete.php
@@ -5,12 +5,12 @@ error_reporting(E_ALL);
 
 if (isset($_SESSION['id'])) {
 	$id = $_REQUEST['id'];
-	
+
 	// 投稿を検査する
 	$messages = $db->prepare('SELECT * FROM posts WHERE id=?');
 	$messages->execute(array($id));
 	$message = $messages->fetch();
-	
+
 	// リツイート元が削除されると、リツイートされた投稿も削除される
 	if ($message['member_id'] === $_SESSION['id']) {
 		// 論理削除
@@ -24,7 +24,7 @@ if (isset($_SESSION['id'])) {
 	// リツイート元及びリツイートされた投稿の全てを削除する
 	if ($message['retweeted_post_id'] > 0) {
 		if ($message['member_id'] === $_SESSION['id']) {
-				// 論理削除
+			// 論理削除
 			$del = $db->prepare('UPDATE posts SET delete_flag=1 WHERE id=? OR retweeted_post_id=? OR id=?');
 			$del->execute(array(
 				$id,
@@ -35,4 +35,5 @@ if (isset($_SESSION['id'])) {
 	}
 }
 
-header('Location: index.php'); exit();
+header('Location: index.php');
+exit();
