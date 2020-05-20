ded5e8eb757a3ff2198b7aa917add918f2ae7e6d
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2020-05-20 14:32:57 +0900

    インデントサイズ指定方法を再設定した上で、php intellisense による整形実施。

--- html/php-challenge/delete.php
+++ html/php-challenge/delete.php
@@ -4,37 +4,36 @@ require('dbconnect.php');
 error_reporting(E_ALL);
 
 if (isset($_SESSION['id'])) {
-	$id = $_REQUEST['id'];
+  $id = $_REQUEST['id'];
 
-	// 投稿を検査する
-	$messages = $db->prepare('SELECT * FROM posts WHERE id=?');
-	$messages->execute(array($id));
-	$message = $messages->fetch();
+  // 投稿を検査する
+  $messages = $db->prepare('SELECT * FROM posts WHERE id=?');
+  $messages->execute(array($id));
+  $message = $messages->fetch();
 
-	// リツイート元が削除されると、リツイートされた投稿も削除される
-	if ($message['member_id'] === $_SESSION['id']) {
-		// 論理削除
-		$del = $db->prepare('UPDATE posts SET delete_flag=1 WHERE id=? OR retweeted_post_id=?');
-		$del->execute(array(
-			$id,
-			$id
-		));
-	}
-	// 自身のツイートのリツイートを削除した際も、
-	// リツイート元及びリツイートされた投稿の全てを削除する
-	if ($message['retweeted_post_id'] > 0) {
-		if ($message['member_id'] === $_SESSION['id']) {
-			// 論理削除
-			$del = $db->prepare('UPDATE posts SET delete_flag=1 WHERE id=? OR retweeted_post_id=? OR id=?');
-			$del->execute(array(
-				$id,
-				$message['retweeted_post_id'],
-				$message['retweeted_post_id']
-			));
-		}
-	}
+  // リツイート元が削除されると、リツイートされた投稿も削除される
+  if ($message['member_id'] === $_SESSION['id']) {
+    // 論理削除
+    $del = $db->prepare('UPDATE posts SET delete_flag=1 WHERE id=? OR retweeted_post_id=?');
+    $del->execute(array(
+      $id,
+      $id
+    ));
+  }
+  // 自身のツイートのリツイートを削除した際も、
+  // リツイート元及びリツイートされた投稿の全てを削除する
+  if ($message['retweeted_post_id'] > 0) {
+    if ($message['member_id'] === $_SESSION['id']) {
+      // 論理削除
+      $del = $db->prepare('UPDATE posts SET delete_flag=1 WHERE id=? OR retweeted_post_id=? OR id=?');
+      $del->execute(array(
+        $id,
+        $message['retweeted_post_id'],
+        $message['retweeted_post_id']
+      ));
+    }
+  }
 }
 
 header('Location: index.php');
 exit();
-
